b5faeafe5c669e66a95fc6461f931cf06de39792
Michael Zeleniuk <zell.xp@gmail.com>
2019-11-22 18:28:21 +0200

    Add OMDb API calls

--- src/components/App.tsx
+++ src/components/App.tsx
@@ -1,16 +1,61 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 
 import Header from "./header";
+import Movie from "./movie";
 import "../styles/App.css";
-
-const search = (searchValue: string) => {
-  console.log(searchValue);
-}
+import "../styles/movies.css";
 
 const App: React.FC = () => {
+  const [loading, setLoading]: Array<boolean | React.Dispatch<React.SetStateAction<any>>> = useState(true);
+  const [movies, setMovies]: Array<any | React.Dispatch<React.SetStateAction<any>>> = useState([]);
+  const [errorMessage, setErrorMessage]: Array<string | React.Dispatch<React.SetStateAction<string>>> = useState("");
+
+  const search = (searchValue: string) => {
+    setLoading(true);
+    setErrorMessage("");
+
+    fetch(`https://www.omdbapi.com/?s=${searchValue}&apikey=4a3b711b`)
+      .then((response: Response) => response.json())
+      .then((jsonResponse: any) => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      })
+  }
+
+  useEffect(() => {
+    fetch("https://www.omdbapi.com/?s=man&apikey=4a3b711b")
+      .then((response: Response) => response.json())
+      .then((jsonResponse: any) => {
+        if (jsonResponse.Response === "True") {
+          setMovies(jsonResponse.Search);
+          setLoading(false);
+        } else {
+          setErrorMessage(jsonResponse.Error);
+          setLoading(false);
+        }
+      })
+  }, []);
+
   return (
     <div className="App">
       <Header search={search} text="Rikka" />
+
+      <div className="movies">
+        {loading && !errorMessage ? (
+          <span>loading...</span>
+        ) : errorMessage ? (
+          <h4 className="error-message">{errorMessage}</h4>
+        ) : (
+          movies.map((movie: any) => (
+            <Movie key={movie.imdbID} movie={movie} />
+          ))
+        )}
+      </div>
     </div>
   );
 }
--- src/components/movie.tsx
+++ src/components/movie.tsx
@@ -4,17 +4,21 @@ import posterPlaceholder from "../images/poster-placeholder.png";
 import "../styles/movie.css";
 
 interface IComponentProps {
-  Poster: string;
-  Title: string;
-  Year: string;
+  movie: {
+    Poster: string;
+    Title: string;
+    Type: string;
+    Year: string;
+  };
 }
 
-const Movie: React.FC<IComponentProps> = (movie: IComponentProps) => {
+const Movie: React.FC<IComponentProps> = (props: IComponentProps) => {
+  const movie: IComponentProps["movie"] = props.movie;
   const posterUrl: string = movie.Poster === "N/A" ? posterPlaceholder : movie.Poster;
 
   return (
     <div className="movie">
-      <h2>{movie.Title}</h2>
+      <h2>{movie.Title} ({movie.Type})</h2>
 
       <img alt={movie.Title} src={posterUrl} />
 
